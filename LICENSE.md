d8c7225fad3df5579c31908189b8295fdb5ec1bf
Jeffrey Meng <jeffkmeng@gmail.com>
2022-11-16 17:25:30 -0800

    feat: initial commit with next

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2022 Jeffrey Meng
+Copyright (c) 2022 João Pedro Schmitz
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
-import * as React from "react";
-import "twin.macro";
-import Layout from "../components/Layout";
+import Head from "next/head";
+import Image from "next/image";
 
-// markup
-const IndexPage = () => {
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+
+export default function Home() {
+    const [count, setCount] = useState(0);
     return (
-        <Layout>
-            <div tw={"pt-6 px-8"}>
-                <h1 tw={"text-3xl font-bold text-center"}>Hello, world!</h1>
-            </div>
-        </Layout>
-    );
-};
+        <div className={styles.container}>
+            <Head>
+                <title>TypeScript starter for Next.js</title>
+                <meta
+                    name="description"
+                    content="TypeScript starter for Next.js that includes all you need to build amazing apps"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-export default IndexPage;
+            <main className={styles.main}>
+                <button onClick={() => setCount(count + 1)}>
+                    Clicked {count} time{count === 1 ? "" : `s`}
+                </button>
+            </main>
+        </div>
+    );
+}
